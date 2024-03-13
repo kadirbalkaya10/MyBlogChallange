92f657e27713f22af386bd35b2ce86792b139f7e
Kadir Balkaya <kadirbalkaya08@gmail.com>
2024-03-12 22:37:58 -0400

    Adding comment for blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,12 +1,20 @@
 // DEPENDENCIES========================
 
+// Landpage right side
 const landpageRight = document.getElementById('landpage-rightSide');
+// Header
 const header = document.getElementById('header');
+// Submit button
 const submit = document.getElementById('submit');
+// Dark theme switcher button
 const btnDarkTheme = document.querySelector('#btn-darktheme');
 
+// When we click button
 btnDarkTheme.addEventListener('click', () => {
+  // Add class ="dark-theme " to header
   header.classList.toggle('dark-theme');
+  //   Toggle class to lanpage right coitainer make it darker, when we click and removes to class from that element.
   landpageRight.classList.toggle('dark-theme');
+  //   Toggle class to submit button to make it blue ,when we click and removes to class from that element.
   submit.classList.toggle('blue-submit');
 });
